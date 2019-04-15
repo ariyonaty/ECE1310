70cf3f97b76b2b9ce11369754b9d2cc6d81b591b
ariyon <ariyonaty@ariyonMacbook.lan1>
2019-04-14 21:50:01 -0700

    Fixing bugs and updating HW

--- Exercises/exercise_85.cpp
+++ Exercises/exercise_85.cpp
@@ -17,14 +17,33 @@ struct Time
 
 int main()
 {
-    struct Time currentTime;
+    struct Time breakfast_time;
+    struct Time lunch_time;
+    struct Time dinner_time;
 
-    std::cout << "Enter hr, min, sec: \n";
-    std::cin >> currentTime.hour >> currentTime.minute >> currentTime.second;
+    std::cout << "Please enter in military time." << std::endl
+              << "Separate numbers using 'space' or 'enter'\n*******\n";
 
-    std::cout << "Current Time: " << std::setfill('0') << std::setw(2)
-              << currentTime.hour << ":" << currentTime.minute << ":"
-              << std::setfill('0') << std::setw(2) << currentTime.second << std::endl;
+    // Ask user to input times for breakfast, lunch, and dinner
+    std::cout << "Enter hour, minute, second for breakfast: ";
+    std::cin >> breakfast_time.hour >> breakfast_time.minute >> breakfast_time.second;
+
+    std::cout << "Enter hour, minute, second for lunch: ";
+    std::cin >> lunch_time.hour >> lunch_time.minute >> lunch_time.second;
+
+    std::cout << "Enter hour, minute, second for dinner: ";
+    std::cin >> dinner_time.hour >> dinner_time.minute >> dinner_time.second;
+
+    // outputs the times for breakfast, lunch, and dinner
+    std::cout << "*******\nBreakfast Time: " << std::setfill('0') << std::setw(2)
+              << breakfast_time.hour << ":" << std::setfill('0') << std::setw(2) << breakfast_time.minute << ":"
+              << std::setfill('0') << std::setw(2) << breakfast_time.second << std::endl;
+    std::cout << "Lunch Time: " << std::setfill('0') << std::setw(2)
+              << lunch_time.hour << ":" << std::setfill('0') << std::setw(2) << lunch_time.minute << ":"
+              << std::setfill('0') << std::setw(2) << lunch_time.second << std::endl;
+    std::cout << "Dinner Time: " << std::setfill('0') << std::setw(2)
+              << dinner_time.hour << ":" << std::setfill('0') << std::setw(2) << dinner_time.minute << ":"
+              << std::setfill('0') << std::setw(2) << dinner_time.second << std::endl;
 
     return 0;
 }
--- Exercises/exercise_86.cpp
+++ Exercises/exercise_86.cpp
@@ -16,19 +16,42 @@ struct StudentScores
 
 int main()
 {
-    struct StudentScores scoreArr[5];
-    // int arrLength = (int)(sizeof(scoreArr)) / sizeof(scoreArr);
+    double score[21] = {84, 66, 77, 78, 89, 92, 61, 91, 65, 72, 70, 85, 97, 65, 84, 79, 92, 76, 86, 79};
+    char id[21] = "ABCDEFGHIJKLMNOPQRST";
 
-    for (int i = 0; i < 5; i++)
+    StudentScores stdScores[20];
+
+    double sum = 0.0;
+    for (int i = 0; i < 20; i++)
     {
-        std::cout << "Student ID and Grade: \n";
-        std::cin >> scoreArr[i].ID >> scoreArr[i].score;
+        stdScores[i].score = score[i];
+        stdScores[i].ID = id[i];
+        sum += stdScores[i].score;
+    }
+    int size = sizeof(stdScores) / sizeof(*stdScores);
+    std::cout << "Average Score: " << sum / size << std::endl;
+
+    char idAbove[size];
+    for (int i = 0; i < size; i++)
+    {
+        if (stdScores[i].score > (sum / size))
+        {
+            idAbove[i] = stdScores[i].ID;
+        }
+        else
+        {
+            idAbove[i] = '-';
+        }
     }
 
-    for (int i = 0; i < 5; i++)
+    std::cout << "Student ID's above average: ";
+    for (int i = 0; i < size; i++)
     {
-        std::cout << "Student ID and Grade: " << scoreArr[i].ID << ", " << scoreArr[i].score << std::endl;
+        std::cout << idAbove[i] << " ";
     }
+    std::cout << std::endl;
 
     return 0;
 }
+
+void averageScore(StudentScores stdScores)
